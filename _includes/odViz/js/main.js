ea8c30e58a7747f44d54bc93de1ced9e5dc721bf
Zhe(Jared) Sun <jaredsun26@gmail.com>
2016-12-07 22:47:03 -0800

    test

--- _includes/odViz/js/main.js
+++ _includes/odViz/js/main.js
@@ -36,33 +36,6 @@ for (var node in centroids){
 
 
 
-
-d3.select("#submit")
-  .on("click", function() {
-  	radio = d3.select("input[name = 'date']:checked").node().value;
-  	switch(radio){
-  		case "all":
-  			day = 31;
-  			break;
-  		case "date":
-  			day =  parseInt(d3.select("#datepicker").node().value.split("-")[2]);
-  			break;
-  		case "day":
-  			day = d3.select("#weekday").node().value;
-  			break;
-  	}
-	subClass = d3.select("#sub").node().value;
-	tripPurp = d3.select("#purpose").node().value;
-
-	fname = day + tripPurp + subClass + '.csv';
-	//console.log(fname)
-	map = d3.select("input[name = 'map']:checked").node().value;
-	d3.select("#chart-area").attr("class", map);
-    onChange(fname);
-});
-
-
-
 function onChange(fname){
 	//reset the charting area
 	d3.select("#chart-area").selectAll("*").remove();
@@ -451,6 +424,31 @@ pieLegend.selectAll("label")
 	}    
 	});
 }
+
+
+d3.select("#submit")
+  .on("click", function() {
+  	radio = d3.select("input[name = 'date']:checked").node().value;
+  	switch(radio){
+  		case "all":
+  			day = 31;
+  			break;
+  		case "date":
+  			day =  parseInt(d3.select("#datepicker").node().value.split("-")[2]);
+  			break;
+  		case "day":
+  			day = d3.select("#weekday").node().value;
+  			break;
+  	}
+	subClass = d3.select("#sub").node().value;
+	tripPurp = d3.select("#purpose").node().value;
+
+	fname = day + tripPurp + subClass + '.csv';
+	//console.log(fname)
+	map = d3.select("input[name = 'map']:checked").node().value;
+	d3.select("#chart-area").attr("class", map);
+    onChange(fname);
+});
 
 onChange("31allall.csv")
 
